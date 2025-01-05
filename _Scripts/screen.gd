2d905946bd57062e051fa53409128e946ae9392a
J2814 <j28x14@gmail.com>
2025-01-05 12:00:52 +0400

    main_menu anchors

--- _Scripts/screen.gd
+++ _Scripts/screen.gd
@@ -2,3 +2,18 @@ extends Node3D
 @onready var screentext =get_node("MeshInstance3D/SubViewport/Label")
 func _process(delta: float) -> void:
 	screentext.set_text(str(clamp(ScoreManager.current_score, 0, 255)) + '$')
+	
+	Global.gameover.emit()
+
+
+func _ready() -> void:
+	Global.gameover.connect(ShowUI)
+
+func _exit_tree() -> void:
+	Global.gameover.disconnect(ShowUI)
+
+
+
+
+func ShowUI():
+	pass
